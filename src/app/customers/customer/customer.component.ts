d5c79e046bc5b2d1a4ab319d17732791e991a767
SAS Ghana <ayisiaddo4@gmail.com>
2018-09-12 07:45:30 +0000

    populate wizard with inputs

--- src/app/customers/customer/customer.component.ts
+++ src/app/customers/customer/customer.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router, Params } from '@angular/router';
+import {ClrWizard} from '@clr/angular';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+
 import { CustomersService } from '../services/customers.service';
 import { Forum } from '../services/data';
 
-import {ClrWizard} from '@clr/angular';
 
 @Component({
   selector: 'app-customer',
@@ -12,9 +14,20 @@ import {ClrWizard} from '@clr/angular';
 })
 export class CustomerComponent implements OnInit {
   @ViewChild('wizardxl') wizardExtraLarge: ClrWizard;
+  @ViewChild('number') numberFi: any;
   // lgOpen: Boolean = false;
   xlOpen: Boolean = false;
   accountype: String = 'Individual Account';
+
+  model = {
+    name: '',
+    favorite: '',
+    number: ''
+  };
+
+  exampleForm = new FormGroup({
+    sample: new FormControl('', Validators.required),
+  });
 
   constructor(
     private route: ActivatedRoute,
